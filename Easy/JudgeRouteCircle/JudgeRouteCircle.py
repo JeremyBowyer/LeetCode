03ab1a715960ca9b470da81bbbde6290bc991a5d
jeremy <jeremybowyer@gmail.com>
2017-09-12 18:35:39 -0400

    Initial commit. Adding Judge Route Circle challenge.

---
+++ Easy/JudgeRouteCircle/JudgeRouteCircle.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        y = 0
+        x = 0
+        
+        for move in moves:
+            if move.lower() not in ['u', 'd', 'l', 'r']:
+                ValueError('Valid moves are "U", "D", "L", or "R". Enter them as a continuous string, not separated by any characters or spaces.')
+            
+            if move.lower() == "u":
+                y += 1
+            elif move.lower() == "d":
+                y -= 1
+            elif move.lower() == "r":
+                x += 1
+            elif move.lower() == "l":
+                x -= 1
+        
+        result = False
+        if x == 0 and y == 0:
+            result = True
+        
+        return result
+        
