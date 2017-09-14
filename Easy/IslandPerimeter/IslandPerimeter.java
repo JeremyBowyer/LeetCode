6eed66ebcbbefa195c80924f156df44a12188063
Jeremy Bowyer <jeremybowyer@gmail.com>
2017-09-14 13:43:19 -0400

    Added Island Perimeter solution, using Java.

---
+++ Easy/IslandPerimeter/IslandPerimeter.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/island-perimeter/description/
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        
+        for (int r = 0; r < grid.length; r++) {
+            for (int d = 0; d < grid[r].length; d++) {
+                
+                int land = grid[r][d];
+                int landT = ((r != 0) ? grid[r-1][d] : 0);
+                int landB = ((r+1 < grid.length) ? grid[r+1][d] : 0);
+                int landL = ((d != 0) ? grid[r][d-1] : 0);
+                int landR = ((d+1 < grid[r].length) ? grid[r][d+1] : 0);
+                
+                if (land == 1) {
+                    perimeter += land - landT;
+                    perimeter += land - landB;
+                    perimeter += land - landL;
+                    perimeter += land - landR;
+                }
+                             
+            }
+        }
+        return perimeter;
+    }
+}
