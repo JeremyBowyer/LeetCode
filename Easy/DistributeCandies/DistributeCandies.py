d04727fc87a8aa48f76aaca831c069d830c2c81a
jeremy <jeremybowyer@gmail.com>
2017-09-13 20:18:52 -0400

    Added Distribute Candies solution, using Python.

---
+++ Easy/DistributeCandies/DistributeCandies.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/distribute-candies/description/
+class Solution:
+    def distributeCandies(self, candies):
+        """
+        :type candies: List[int]
+        :rtype: int
+        """
+        return min(len(candies) // 2, len(set(candies)))
